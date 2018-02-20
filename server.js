000b6ed7bbeb191684d13e9fad5f5c78710329c4
VRKarthik <karthikeyangvp@gmail.com>
2018-02-20 07:45:53 +0530

    started implementing node server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = new express();
+
+app.listen(3000, function() {
+    console.log('server started and listening on port 3000');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
